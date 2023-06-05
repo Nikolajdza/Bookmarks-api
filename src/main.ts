d5099103b4bc421aae1780aee8e1dbbb37da3ada
Nikola Buljcik <nikola.buljcik@itengine.rs>
2023-06-05 19:41:00 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookmarks API')
+    .setDescription('The bookmarks API description')
+    .setVersion('1.0')
+    .addTag('bookmarks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3333);
+}
+
+bootstrap();
